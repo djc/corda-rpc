87c6e0d1acbb1a5ab57e9f7e638218dae5bc5704
Dirkjan Ochtman <dirkjan.ochtman@ing.com>
2019-12-31 11:07:23 +0100

    Implement AMQP types required for Corda RPC

--- oasis-amqp/examples/connect.rs
+++ oasis-amqp/examples/connect.rs
@@ -3,8 +3,10 @@ use std::convert::TryFrom;
 use std::time::SystemTime;
 
 use futures::{sink::SinkExt, stream::StreamExt};
-use oasis_amqp::{amqp, sasl, Codec, Frame, Protocol};
+use oasis_amqp::{amqp, sasl, ser, Codec, Described, Frame, Protocol};
+use oasis_amqp_derive::amqp as amqp_derive;
 use rand::{self, Rng};
+use serde::{Deserialize, Serialize};
 use serde_bytes::{ByteBuf, Bytes};
 use tokio;
 use tokio::net::TcpStream;
@@ -115,6 +117,39 @@ async fn main() {
     properties.insert("rpc-session-id-timestamp", amqp::Any::I64(timestamp));
     properties.insert("deduplication-sequence-number", amqp::Any::I64(0));
 
+    let mut body = vec![];
+    body.extend_from_slice(CORDA_MAGIC);
+    body.push(SectionId::DataAndStop as u8);
+    let envelope = Envelope {
+        blob: Bytes::new(AMQP_EMPTY_LIST),
+        schema: Schema(vec![
+            TypeNotation::CompositeType(CompositeType {
+                name: "Route",
+                label: None,
+                provides: vec!["foo"],
+                descriptor: Descriptor {
+                    name: None,
+                    code: None,
+                },
+                fields: vec![
+                    Field {
+                        name: "address",
+                        ty: "ulong",
+                        requires: vec![
+                            "foo"
+                        ],
+                        default: None,
+                        label: None,
+                        mandatory: false,
+                        multiple: false,
+                    }
+                ],
+            })
+        ]),
+    };
+    ser::into_bytes(&envelope, &mut body).unwrap();
+    println!("body: {:?}", body);
+
     let transfer = Frame::Amqp(amqp::Frame {
         channel: 0,
         extended_header: None,
@@ -141,4 +176,129 @@ async fn main() {
     transport.send(transfer).await.unwrap();
     let transferred = transport.next().await.unwrap().unwrap();
     println!("read: {:#?}\n", transferred);
+
+    /*
+    let attach = Frame::Amqp(amqp::Frame {
+        channel: 0,
+        extended_header: None,
+        performative: amqp::Performative::Attach(amqp::Attach {
+            name: message_id.clone(),
+            handle: 1,
+            role: amqp::Role::Receiver,
+            snd_settle_mode: None,
+            rcv_settle_mode: None,
+            source: Some(amqp::Source {
+                address: Some("vx-web"),
+                ..Default::default()
+            }),
+            target: Some(amqp::Target {
+                address: Some("rpc.client".into()),
+                ..Default::default()
+            }),
+            unsettled: None,
+            incomplete_unsettled: None,
+            initial_delivery_count: None,
+            max_message_size: None,
+            offered_capabilities: None,
+            desired_capabilities: None,
+            properties: None,
+        }),
+        message: None,
+    });
+
+    println!("send transfer: {:#?}", attach);
+    transport.send(attach).await.unwrap();
+    */
+    let next = transport.next().await;
+    println!("read: {:#?}\n", next);
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+enum SectionId {
+    DataAndStop,
+    AltDataAndStop,
+    Encoding,
+}
+
+#[amqp_derive(descriptor("corda:envelope:list", 0xc5620000_00000001))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:envelope:list")]
+struct Envelope<'a> {
+    #[serde(borrow)]
+    pub blob: &'a Bytes,
+    pub schema: Schema<'a>,
+}
+
+#[amqp_derive(descriptor("corda:schema:list", 0xc5620000_00000002))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:schema:list")]
+struct Schema<'a>(#[serde(borrow)] Vec<TypeNotation<'a>>);
+
+#[amqp_derive(descriptor("corda:object-descriptor:list", 0xc5620000_00000003))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:object-descriptor:list")]
+struct Descriptor<'a> {
+    name: Option<&'a str>,
+    code: Option<u64>,
+}
+
+#[amqp_derive(descriptor("corda:field:list", 0xc5620000_00000004))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:field:list")]
+struct Field<'a> {
+    name: &'a str,
+    #[serde(rename = "type")]
+    ty: &'a str,
+    #[serde(borrow)]
+    requires: Vec<&'a str>,
+    default: Option<&'a str>,
+    label: Option<&'a str>,
+    mandatory: bool,
+    multiple: bool,
+}
+
+#[amqp_derive]
+#[derive(Debug, Serialize)]
+enum TypeNotation<'a> {
+    CompositeType(CompositeType<'a>),
+    RestrictedType(RestrictedType<'a>),
+}
+
+#[amqp_derive(descriptor("corda:composite-type:list", 0xc5620000_00000005))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:composite-type:list")]
+struct CompositeType<'a> {
+    name: &'a str,
+    label: Option<&'a str>,
+    provides: Vec<&'a str>,
+    descriptor: Descriptor<'a>,
+    fields: Vec<Field<'a>>,
+}
+
+#[amqp_derive(descriptor("corda:restricted-type:list", 0xc5620000_00000006))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:restricted-type:list")]
+struct RestrictedType<'a> {
+    name: &'a str,
+    label: Option<&'a str>,
+    provides: Vec<&'a str>,
+    source: &'a str,
+    descriptor: Descriptor<'a>,
+    choices: Vec<Choice<'a>>,
+}
+
+#[amqp_derive(descriptor("corda:choice:list", 0xc5620000_00000007))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:choice:list")]
+struct Choice<'a> {
+    name: &'a str,
+    value: &'a str,
+}
+
+#[amqp_derive(descriptor("corda:transforms-schema:list", 0xc5620000_00000009))]
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename = "corda:transforms-schema:list")]
+struct TransformsSchema {}
+
+const CORDA_MAGIC: &[u8; 7] = b"corda\x01\x00";
+const AMQP_EMPTY_LIST: &[u8] = &[45];
