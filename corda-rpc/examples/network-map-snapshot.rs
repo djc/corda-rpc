ddc811d81a56ac933867b01ba60a5f5e8a2458a0
Dirkjan Ochtman <dirkjan.ochtman@ing.com>
2020-04-08 10:01:29 +0200

    Remove hard-coded configuration from example code

--- corda-rpc/examples/network-map-snapshot.rs
+++ corda-rpc/examples/network-map-snapshot.rs
@@ -5,6 +5,7 @@ use std::time::SystemTime;
 use oasis_amqp::{amqp, proto::Frame, Client};
 use rand::{self, Rng};
 use serde_bytes::{ByteBuf, Bytes};
+use structopt::StructOpt;
 use tokio;
 use uuid::Uuid;
 
@@ -14,20 +15,24 @@ use corda_rpc::{
 
 #[tokio::main]
 async fn main() {
-    let mut client = Client::connect("localhost:10006").await.unwrap();
-    client.login("vxdir", "vxdir").await.unwrap();
-    client.open("vxweb").await.unwrap();
+    let options = Options::from_args();
+    let mut client = Client::connect(&options.address).await.unwrap();
+    client
+        .login(&options.user, &options.password)
+        .await
+        .unwrap();
+    client.open(CONTAINER).await.unwrap();
     client.begin().await.unwrap();
 
     client
         .attach(amqp::Attach {
-            name: "vx-web-sender".into(),
+            name: "corda-rpc-sender".into(),
             handle: 0,
             role: amqp::Role::Sender,
             snd_settle_mode: None,
             rcv_settle_mode: None,
             source: Some(amqp::Source {
-                address: Some("vx-web".into()),
+                address: Some(CONTAINER.into()),
                 ..Default::default()
             }),
             target: Some(amqp::Target {
@@ -46,7 +51,8 @@ async fn main() {
         .unwrap();
 
     let rcv_queue_name = format!(
-        "rpc.client.vxdir.{}",
+        "rpc.client.{}.{}",
+        options.user,
         rand::thread_rng().gen::<u64>() & 0xefffffff_ffffffff,
     );
 
@@ -62,7 +68,7 @@ async fn main() {
                 ..Default::default()
             }),
             target: Some(amqp::Target {
-                address: Some("vx-web".into()),
+                address: Some(CONTAINER.into()),
                 ..Default::default()
             }),
             unsettled: None,
@@ -102,7 +108,10 @@ async fn main() {
     let delivery_tag = Uuid::new_v4();
 
     let mut properties = HashMap::new();
-    properties.insert("_AMQ_VALIDATED_USER", amqp::Any::Str("vxdir".into()));
+    properties.insert(
+        "_AMQ_VALIDATED_USER",
+        amqp::Any::Str((&options.user).into()),
+    );
     properties.insert("tag", amqp::Any::I32(0));
     properties.insert("method-name", amqp::Any::Str("networkMapSnapshot".into()));
     properties.insert("rpc-id", amqp::Any::Str(rpc_id.clone().into()));
@@ -146,7 +155,7 @@ async fn main() {
                 properties: Some(amqp::Properties {
                     message_id: Some(rpc_id.clone().into()),
                     reply_to: Some(rcv_queue_name.clone().into()),
-                    user_id: Some(Bytes::new(b"vxdir")),
+                    user_id: Some(Bytes::new(options.user.as_bytes())),
                     ..Default::default()
                 }),
                 application_properties: Some(amqp::ApplicationProperties(properties)),
@@ -171,3 +180,14 @@ async fn main() {
     let rsp = Envelope::<Try<amqp::List<NodeInfo>, ()>>::decode(&body).unwrap();
     println!("{:#?}", rsp.obj);
 }
+
+#[derive(Debug, StructOpt)]
+struct Options {
+    #[structopt(short, long)]
+    user: String,
+    #[structopt(short, long)]
+    password: String,
+    address: String,
+}
+
+const CONTAINER: &str = "corda-rpc";
